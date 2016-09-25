5129d0f793c7a193d9649727b771d7d2a46a7a97
Ibrahim Itani <andrahim@gmail.com>
2016-09-25 13:52:02 -0700

    fixed shopify app initializer

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,6 @@ ShopifyApp.configure do |config|
 
   config.api_key = "26d64dc112f5657968e26b4c8789ddf9"
   config.secret = "089cd4233dd3431c54fdc49e90d70b3a"
-  config.redirect_url = "https://test-dash-app.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
