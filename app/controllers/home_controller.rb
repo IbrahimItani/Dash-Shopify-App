d8ed5a9e273e9b12148196f1ee79e56a8d52ca04
Ibrahim Itani <andrahim@gmail.com>
2016-09-26 00:35:03 -0700

    Testing with Orders Now in App Dashboard..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @orders = ShopifyAPI::Order.find(:all, params: {limit: 5, :order => "subtotal_price ASC"})
+    @orders = ShopifyAPI::Order.find(:all).order(total_price: :desc)
   end
 end
