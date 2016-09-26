b1e4a81f755b0fdd695ea64f9ca6cac9e28bff72
Ibrahim Itani <andrahim@gmail.com>
2016-09-25 17:48:08 -0700

    Initial Form Setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @articles = ShopifyAPI::Article.find(:all)
   end
 end
