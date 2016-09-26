b8036f0e352fd3278b3bff09e8fe9fd066c2ce14
Ibrahim Itani <andrahim@gmail.com>
2016-09-26 16:26:24 -0700

    Abstract API Key and Secret

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
 
-  config.api_key = "26d64dc112f5657968e26b4c8789ddf9"
-  config.secret = "089cd4233dd3431c54fdc49e90d70b3a"
+  config.api_key = "ENV_API_KEY"
+  config.secret = "ENV_SECRET"
   config.scope = "read_orders, read_products, read_content, write_content"
   config.embedded_app = true
 end
