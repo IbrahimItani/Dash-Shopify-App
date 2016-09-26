6936878bd764e0de538baff2ddb8455a3fcf5347
Ibrahim Itani <andrahim@gmail.com>
2016-09-25 23:40:38 -0700

    TESTING for Orders

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @customers = ShopifyAPI::Customer.find(:all, params: { limit: 10 })
-    @orders = ShopifyAPI::Order.find(:all, :params => {:customer_id => @customers.id, :order => "subtotal_price ASC" })
+    @orders = ShopifyAPI::Order.find(:all, :order => "subtotal_price ASC")
 
   end
 end
