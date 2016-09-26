777b56b8772433815fb561a69543967e3c5594af
Ibrahim Itani <andrahim@gmail.com>
2016-09-25 19:04:50 -0700

    TESTING JS File Again

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    ShopifyAPI::ScriptTag.create({src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js";}) 
+    ShopifyAPI::ScriptTag.create(:event => "onload", :src => "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js")
   end
 end
