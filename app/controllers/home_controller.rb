8576cc54f9536f6b4b8de43b49b7f2ae359d795d
Ibrahim Itani <andrahim@gmail.com>
2016-09-25 17:30:59 -0700

    Added more article implementation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @articles = ShopifyAPI::Articles.find(:all, params: {limit: 10})
+    @articles = ShopifyAPI::Articles.find(:all)
   end
 end
