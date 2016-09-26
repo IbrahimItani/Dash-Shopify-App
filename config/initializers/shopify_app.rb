8bb1f2a083f4248cb0c7c588978e882b9b0201f1
Ibrahim Itani <andrahim@gmail.com>
2016-09-26 00:06:50 -0700

    Trying Products as Reference

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,6 @@ ShopifyApp.configure do |config|
 
   config.api_key = "26d64dc112f5657968e26b4c8789ddf9"
   config.secret = "089cd4233dd3431c54fdc49e90d70b3a"
-  config.scope = "read_orders, read_customers, read_products"
+  config.scope = "read_orders, read_products, read_content, write_content"
   config.embedded_app = true
 end
