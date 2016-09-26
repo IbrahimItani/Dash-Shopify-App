899918e29207a782120fe2b0284b3d83767b1a44
Ibrahim Itani <andrahim@gmail.com>
2016-09-25 23:10:05 -0700

    TESTING for Abandoned Checkouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @abandonedCheckouts = ShopifyAPI::AbandonedCheckout.find(:all, params: { limit: 10 })
+    @abandonedCheckouts = ShopifyAPI::AbandonedCheckout.find(:all)
   end
 end
