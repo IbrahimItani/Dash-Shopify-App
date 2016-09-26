dfe48168f673425a62d76cb7e3eda79da8e086b0
Ibrahim Itani <andrahim@gmail.com>
2016-09-25 18:50:11 -0700

    TESTING

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    ShopifyAPI::ScriptTag.create({event: "onload", src: "https://ibrahimitani.com/dash-app.js";}) 
+    # ShopifyAPI::ScriptTag.create({event: "onload", src: "https://ibrahimitani.com/dash-app.js";}) 
   end
 end
