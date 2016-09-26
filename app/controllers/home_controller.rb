a37ff4954a31714548e8c8cd889b6711e64a0eaf
Ibrahim Itani <andrahim@gmail.com>
2016-09-26 01:52:32 -0700

    Testing with Orders Now in App Dashboard..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     # @orders = ShopifyAPI::Order.find(:all)
-    @orders = ShopifyAPI::Order.find(:all, :params => {limit: 5, order: "processed_at DESC"}).map(&:processed_at)
+    @orders = ShopifyAPI::Order.find(:all, :params => {limit: 5, order: "created_at DESC"}).map(&:created_at)
 
 
   end
