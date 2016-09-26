24fb6727c18deade1a1258fdf866cb89d934f525
Ibrahim Itani <andrahim@gmail.com>
2016-09-25 23:51:28 -0700

    Fixed Order Syntax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @orders = ShopifyAPI::Order.find(:all, params: {limit: 10})
+    # @orders = ShopifyAPI::Order.find(:all, params: {limit: 10})
   end
 end
