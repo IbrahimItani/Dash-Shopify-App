874099bafae2ca57114bb726fab258e367873336
Ibrahim Itani <andrahim@gmail.com>
2016-09-26 02:03:39 -0700

    Testing with Orders Now in App Dashboard..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    # @orders = ShopifyAPI::Order.find(:all)
-    @orders = ShopifyAPI::Order.find(:all, :params => {limit: 5, order: total_price}).map(&:total_price)
-
+    @orders = ShopifyAPI::Order.find(:all)
 
   end
 
